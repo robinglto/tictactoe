c6c3fadf7dd03ac0c774edb40ae62744100980dc
robinglto <robinsomedia@pm.me>
2023-08-11 22:52:13 -0400

    wip

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,36 +1,38 @@
+"use client";
 import React from "react";
+import { useState } from "react";
+
+function Square({ value }: any) {
+  // const [value, setValue] = useState("");
+
+  // function handleClick() {
+  //   setValue("X");
+  // }
+  return (
+    <div>
+      <button
+        className="bg-white rounded-lg text-black py-1 px-2 m-0.5 w-12 h-12"
+        // onClick={handleClick}
+      >
+        {value}
+      </button>
+    </div>
+  );
+}
 
 export default function Board() {
+  const [square, setSquare] = useState(Array(9).fill(""));
+
+  const buttons = Array.from({ length: 9 }, (_, index) => index + 1);
+
   return (
     <div className="flex justify-center h-screen items-center">
       <div className="grid grid-cols-3 w-1/3">
-        <button className="bg-white rounded-lg text-black py-1 px-4 m-1.5">
-          1
-        </button>
-        <button className="bg-white rounded-lg text-black py-1 px-4 m-1.5">
-          2
-        </button>
-        <button className="bg-white rounded-lg text-black py-1 px-4 m-1.5">
-          3
-        </button>
-        <button className="bg-white rounded-lg text-black py-1 px-4 m-1.5">
-          4
-        </button>
-        <button className="bg-white rounded-lg text-black py-1 px-4 m-1.5">
-          5
-        </button>
-        <button className="bg-white rounded-lg text-black py-1 px-4 m-1.5">
-          6
-        </button>
-        <button className="bg-white rounded-lg text-black py-1 px-4 m-1.5">
-          7
-        </button>
-        <button className="bg-white rounded-lg text-black py-1 px-4 m-1.5">
-          8
-        </button>
-        <button className="bg-white rounded-lg text-black py-1 px-4 m-1.5">
-          9
-        </button>
+        {buttons.map((index) => (
+          <div key={index}>
+            <Square value={square[index]} />
+          </div>
+        ))}
       </div>
     </div>
   );
